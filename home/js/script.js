bf67b6973bfb2dd5301d257469e9075ed2de2e80
MohammadBahaa1 <MohammadBahaa_2001mb@outlook.com>
2020-05-31 06:37:32 +0200

    added: shop route & side-navbar

--- home/js/script.js
+++ home/js/script.js
@@ -1,21 +1,22 @@
 // First slider
-$('.slider-one')
-.not(".slick-intialized")
-.slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    prevArrow: '.site-slider .slider-btn .prev',
-    nextArrow: '.site-slider .slider-btn .next'
+$(".slider-one").not(".slick-intialized").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
 });
 
 // Second slider
-$('.slider-two')
-.not(".slick-intialized")
-.slick({
-    prevArrow: '.site-slider-two .prev',
-    nextArrow: '.site-slider-two .next',
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplaySpeed: 3000
+$(".slider-two").not(".slick-intialized").slick({
+  prevArrow: ".site-slider-two .prev",
+  nextArrow: ".site-slider-two .next",
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplaySpeed: 3000,
+});
+
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
 });
